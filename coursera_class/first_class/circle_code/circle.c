9dc0c461edeedaef6acc3d94ae0b1ed7427f22d1
Maciej Gieparda <macgie29@gmail.com>
2023-04-17 22:27:51 +0200

    circle_upd

--- coursera_class/first_class/circle_code/circle.c
+++ coursera_class/first_class/circle_code/circle.c
@@ -7,7 +7,7 @@ int main()
     double area = 0.0, radius = 0.0;
     printf("Enter radius: ");
     scanf("%lf", &radius);
-    area = PI * radius * radius;
+    area = PI * (radius * radius);
     printf("radius of %lf meters; area is %lf sq. meters\n", radius, area);
     return 0;
 }
